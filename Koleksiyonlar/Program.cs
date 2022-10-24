8b4b591899fa1f04d81ff40effb6bc3907c12b52
hasanyannar <hasanyannar@gmail.com>
2022-10-24 12:54:31 +0300

    24.10.2022

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = new string[] {"hasan", "mehmet", "musa", "kamil" };
+            Console.WriteLine(strings[0]);
+
+            List<string> isimler2 = new List<string> { "engin", "murat", "kerem", "halil" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("hasan");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+
+        }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OOP1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 3;
+
+            ProductManager ProductManager = new ProductManager();
+            ProductManager.Add(product1);
+            Console.WriteLine(product1.ProductName);
+        }
+    }
+}
